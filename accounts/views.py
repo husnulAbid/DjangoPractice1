822d6a83b83a95e5a106a22371b0ff415be695fa
husnulAbid <31898066+husnulAbid@users.noreply.github.com>
2020-11-03 17:03:54 +0600

    backend in function

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,38 @@ from math import sqrt
 from .models import operations
 
 
-def index(request):
+def fact(number):
+    i = number
+    result = 1
+    while i:
+        result = result * i
+        i = i - 1
+
+    return result
+
+
+def fibbo(number):
+    a = 0
+    b = 1
     result = 0
+
+    if number == 1:
+        result = 0
+    elif number == 2:
+        result = 1
+    else:
+        i = 3
+        while i <= number:
+            result = a + b
+            a = b
+            b = result
+            i = i + 1
+
+    return result
+
+
+def index(request):
+
     if request.POST:
         number = int(request.POST['num1'])
 
@@ -23,31 +53,13 @@ def index(request):
             return render(request, "inputPage.html", {"result": result})
 
         elif 'fact' in request.POST:
-            i = number
-            result = 1
-            while i:
-                result = result * i
-                i = i - 1
-
+            result = fact(number)
             MyOperations = operations(type='fact', input=number, result=result)
             MyOperations.save()
             return render(request, "inputPage.html", {"result": result})
 
         elif 'fibbo' in request.POST:
-            a = 0
-            b = 1
-            if number == 1:
-                result = 0
-            elif number == 2:
-                result = 1
-            else:
-                i = 3
-                while i <= number:
-                    result = a + b
-                    a = b
-                    b = result
-                    i = i + 1
-
+            result = fibbo(number)
             MyOperations = operations(type='fibbo', input=number, result=result)
             MyOperations.save()
             return render(request, "inputPage.html", {"result": result})
